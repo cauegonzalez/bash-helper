e1810f094c4e209cad8ff09da11d685720e97f54
Cauê Gonzalez <cauegonzalez@gmail.com>
2023-01-30 15:03:31 -0500

    Update bash-install.sh

--- bash-install.sh
+++ bash-install.sh
@@ -12,4 +12,14 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
 # Other requirements
-apt install python-is-python3 ghc gcc g++ php ruby perl swi-prolog elixir wireshark mit-scheme -s
+apt install python-is-python3 -s 
+apt install ghc -s 
+apt install gcc -s 
+apt install g++ -s
+apt install php -s
+apt install ruby -s 
+apt install perl -s 
+apt install swi-prolog -s
+apt install elixir -s 
+apt install wireshark -s
+apt install mit-scheme -s
